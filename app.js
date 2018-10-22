7e3a982d6777f9d8c8fbcb325238a6ed18b2ba26
Anna Peterson <peterson.anna.c@gmail.com>
2018-10-22 09:24:46 -0700

    Use ejs engine instead of jade

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
