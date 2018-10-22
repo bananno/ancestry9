f49415c99dc1f2fc0196c8784131416600be9667
Anna Peterson <peterson.anna.c@gmail.com>
2018-10-22 14:13:28 -0700

    Change name of persons attribute for events

--- model/events.js
+++ model/events.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var eventSchema = new mongoose.Schema({
   title: String,
   date: { type: Date, default: null },
-  persons: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Person' }]
+  people: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Person' }]
 });
 
 mongoose.model('Event', eventSchema);
