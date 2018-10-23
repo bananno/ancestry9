5f7a2020c51ba238d440bdabb3545c198dbf2051
Anna Peterson <peterson.anna.c@gmail.com>
2018-10-23 13:41:54 -0700

    Include new location components

--- tools/getLocationValues.js
+++ tools/getLocationValues.js
@@ -2,7 +2,15 @@
 function getLocationValues(req) {
   var location = {
     country: req.body.location_country,
+    region1: req.body.location_region1,
+    region2: req.body.location_region2,
+    city: req.body.location_city,
+    notes: req.body.location_notes,
   };
+
+  if (location.country == 'United States') {
+    location.region1 = req.body.location_region1_usa;
+  }
 
   return location;
 }
