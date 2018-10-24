5fbfa20f9741c2f8793a9fb236a1c577ffe5d07d
Anna Peterson <peterson.anna.c@gmail.com>
2018-10-24 13:49:58 -0700

    Add view for person sources

--- routes/person.js
+++ routes/person.js
@@ -10,6 +10,7 @@ var getNewEventValues = require('../tools/getNewEventValues');
 convertParamPersonId();
 
 router.get('/:personId', makeRouteGet('none'));
+router.get('/:personId/sources', personSources);
 
 router.get('/:personId/addEvent', makeRouteGet('events'));
 router.post('/:personId/addEvent', makeRouteEditPost('events'));
@@ -255,4 +256,63 @@ function makeRouteDelete(editField, corresponding) {
        }
     });
   };
+}
+
+function personSources(req, res, ntext) {
+  mongoose.model('Person').findById(req.personId)
+  .populate('parents')
+  .populate('spouses')
+  .populate('children')
+  .exec(function(err, person) {
+    mongoose.model('Person')
+    .find({})
+    .exec(function(err, allPeople) {
+      mongoose.model('Event')
+      .find({ people: person })
+      .populate('people')
+      .exec(function(err, events) {
+        mongoose.model('Citation')
+        .find({ person: person })
+        .populate('source')
+        .exec(function(err, citations) {
+
+          var people = removePersonFromList(allPeople, person);
+
+          var siblings = [];
+
+          if (person.parents.length > 0) {
+            siblings = people.filter(function(thisPerson) {
+              for (var i = 0; i < thisPerson.parents.length; i++) {
+                var thisParent1 = thisPerson.parents[i];
+                for (var j = 0; j < person.parents.length; j++) {
+                  var thisParent2 = person.parents[j];
+                  if (thisParent1 == thisParent2.id) {
+                    return true;
+                  }
+                }
+              }
+              return false;
+            });
+          }
+
+          events = sortEvents(events);
+          citations = sortCitations(citations, 'item');
+
+          res.format({
+            html: function() {
+              res.render('people/sources', {
+                personId: req.personId,
+                person: person,
+                people: people,
+                siblings: siblings,
+                events: events,
+                editView: 'none',
+                citations: citations,
+              });
+            }
+          });
+        });
+      });
+    });
+  });
 }
