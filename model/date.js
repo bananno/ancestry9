4f0fc2b2ad85fdd9f2a85df802ffe2852eee48ab
Anna Peterson <peterson.anna.c@gmail.com>
2018-10-24 17:40:04 -0700

    Add a display attribute for non-specific dates

--- model/date.js
+++ model/date.js
@@ -3,4 +3,5 @@ module.exports = {
   year: { type: Number, default: 0},
   month: { type: Number, default: 0},
   day: { type: Number, default: 0},
+  display: String,
 };
--- tools/getDateValues.js
+++ tools/getDateValues.js
@@ -4,6 +4,7 @@ function getDateValues(req) {
     year: req.body.date_year,
     month: req.body.date_month,
     day: req.body.date_day,
+    display: req.body.date_display,
   };
 
   return date;
