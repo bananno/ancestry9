e4a6ba4a96008f1e639afc18169993811dfefac8
Anna Peterson <peterson.anna.c@gmail.com>
2018-11-01 12:08:24 -0700

    Remove unused function

--- routes/event.js
+++ routes/event.js
@@ -129,13 +129,3 @@ function deleteEvent(req, res) {
     });
   });
 }
-
-// HELPER
-
-function isSamePerson(person1, person2) {
-  var id1 = person1._id ? person1._id : person1;
-  var id2 = person2._id ? person2._id : person2;
-  id1 = '' + id1;
-  id2 = '' + id2;
-  return id1 == id2;
-}
