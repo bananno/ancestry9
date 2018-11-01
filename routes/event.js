4f4f977ec743349379ddf16c0d966d6cc4061821
Anna Peterson <peterson.anna.c@gmail.com>
2018-11-01 10:52:57 -0700

    Add ability to reorder people for events

--- routes/event.js
+++ routes/event.js
@@ -15,6 +15,7 @@ router.post('/:eventId/edit/date', makeRouteEditPost('date'));
 router.get('/:eventId/add/people', makeRouteEditGet('people'));
 router.post('/:eventId/add/people', makeRouteEditPost('people'));
 router.post('/:eventId/delete/people/:deleteId', makeRouteDelete('people'));
+router.post('/:eventId/reorder/people/:orderId', makeRouteReorder('people'));
 router.get('/:eventId/edit/location', makeRouteEditGet('location'));
 router.post('/:eventId/edit/location', makeRouteEditPost('location'));
 router.get('/:eventId/edit/notes', makeRouteEditGet('notes'));
@@ -95,6 +96,36 @@ function makeRouteDelete(editField) {
   };
 }
 
+function makeRouteReorder(attr) {
+  return function(req, res) {
+    var eventId = req.params.eventId;
+    mongoose.model('Event').findById(eventId, function(err, event) {
+      var updatedObj = {};
+      updatedObj[attr] = event[attr];
+      var orderId = req.params.orderId;
+
+      if (attr == 'people') {
+        for (var i = 1; i < updatedObj[attr].length; i++) {
+          var thisPerson = updatedObj[attr][i];
+          if (isSamePerson(thisPerson, orderId)) {
+            updatedObj[attr][i] = updatedObj[attr][i - 1];
+            updatedObj[attr][i - 1] = thisPerson;
+            break;
+          }
+        }
+      }
+
+      event.update(updatedObj, function(err) {
+        res.format({
+          html: function() {
+            res.redirect('/event/' + eventId);
+          }
+        });
+      });
+    });
+  };
+}
+
 function deleteEvent(req, res) {
   var eventId = req.params.eventId;
   mongoose.model('Event').findById(eventId, function(err, event) {
@@ -106,4 +137,14 @@ function deleteEvent(req, res) {
       });
     });
   });
+}
+
+// HELPER
+
+function isSamePerson(person1, person2) {
+  var id1 = person1._id ? person1._id : person1;
+  var id2 = person2._id ? person2._id : person2;
+  id1 = '' + id1;
+  id2 = '' + id2;
+  return id1 == id2;
 }
