bb0a5b8700743e993e7401fbb31f0857106cba40
Anna Peterson <peterson.anna.c@gmail.com>
2018-11-05 15:52:20 -0800

    Replace more code using the reusable filter function instead

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -20,31 +20,6 @@ function filterPersonList() {
   }
 }
 
-function filterRelativeList() {
-  var $buttons = $('.relative-list .filter-button');
-  var $allRows = $('.relative-list .person-row');
-  console.log($allRows.length)
-
-  $buttons.toArray().forEach(button => {
-    var current = true;
-    var $button = $(button);
-    var type = $button.attr('relative-type');
-    var $rows = $allRows.filter('[relative-type="' + type + '"]');
-    console.log($rows.length)
-    $button.click(() => {
-      current = !current;
-      if (current) {
-        console.log('show ' + type);
-        $button.addClass('active');
-      } else {
-        console.log('hide ' + type);
-        $button.removeClass('active');
-      }
-      $rows.toggle(current);
-    });
-  });
-}
-
 function toggleRegionUsState() {
   var $country = $('[name="location_country"]');
   var $usaRegion = $('[name="location_region1_usa"]');
