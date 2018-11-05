85a100c7e21ad25b4566278242eabd01ee3c8d9d
Anna Peterson <peterson.anna.c@gmail.com>
2018-11-05 15:35:16 -0800

    Add filter to toggle visibility for sources; create reusable filter functionality

---
+++ public/scripts/filterToggle.js
@@ -0,0 +1,46 @@
+
+/*
+
+  BUTTONS:
+
+  <div class="filter-buttons" filter="putNameHere">
+      <div class="filter-button active" filter-set="thing1">
+          thing 1 <!-- currently active -->
+      </div>
+      <div class="filter-button" value="thing2">
+          thing 2 <!-- currently NOT active -->
+      </div>
+  </div>
+
+  ITEMS TOGGLED:
+
+  <div filter="putNameHere" filter-set="thing1"></div>
+  <div filter="putNameHere" filter-set="thing1"></div>
+  <div filter="putNameHere" filter-set="thing2"></div>
+  <div filter="putNameHere" filter-set="thing2"></div>
+
+*/
+
+function createToggleFilter(filterName) {
+  var $buttonSet = $('.filter-buttons[filter="' + filterName + '"] .filter-button');
+  var $items = $('[filter="' + filterName + '"]').not($buttonSet);
+
+  $buttonSet.toArray().forEach(button => {
+    var $button = $(button);
+    var set = $button.attr('filter-set');
+    var $setItems = $items.filter('[filter-set="' + set + '"]');
+    var active = $button.hasClass('active');
+    if (!active) {
+      $setItems.hide();
+    }
+    $button.click(() => {
+      active = !active;
+      if (active) {
+        $button.addClass('active');
+      } else {
+        $button.removeClass('active');
+      }
+      $setItems.toggle(active);
+    });
+  });
+}
