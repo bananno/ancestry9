b6b53ee146f8d00d950d240e9d2e5b31eb7a9c27
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-22 14:30:37 -0800

    Change view engine from html/ejs to only ejs

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
