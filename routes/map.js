8bad0420c03832f2f8ff610bbffa3725992b4d95
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-07 18:51:37 -0800

    Continue building map

--- routes/map.js
+++ routes/map.js
@@ -1,11 +1,35 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var router = express.Router();
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
 
 router.get('/', showMap);
 
 function showMap(req, res, next) {
-  res.redirect('/');
+  getEventsAndSources((error, sources, events) => {
+    if (error) {
+      return next(error);
+    }
+    res.render('layout', {
+      view: 'map/index',
+      title: 'Map',
+      events: events,
+      sources: sources,
+    });
+  });
+}
+
+function getEventsAndSources(callback) {
+  mongoose.model('Source')
+  .find({})
+  .populate('people')
+  .exec((error1, sources) => {
+    mongoose.model('Event')
+    .find({})
+    .populate('people')
+    .exec((error2, events) => {
+      callback(error1 || error2, sources, events);
+    });
+  });
 }
 
 module.exports = router;
