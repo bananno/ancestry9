3c3005acb51fcbd3145236041187bc83c9c42f6a
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-12 16:57:48 -0700

    Move to-do list routes to own file

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 const routeFiles = [
   'index',
+  'to-do',
   'database',
 ];
 
---
+++ routes/to-do.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+module.exports = router;
+
+router.get('/to-do', showToDoList);
+router.post('/to-do/new', newToDoItem);
+router.post('/to-do/:id/edit', editToDoItem);
+
+function showToDoList(req, res, next) {
+  mongoose.model('To-do').find({}, (err, todoItems) => {
+    res.render('layout', {
+      view: 'to-do',
+      title: 'To-do List',
+      todoItems: todoItems,
+    });
+  });
+}
+
+function newToDoItem(req, res, next) {
+  const items = req.body.items.split('\n')
+    .map(item => item.trim())
+    .filter(item => item);
+
+  const newTodo = {
+    items: items,
+  };
+
+  mongoose.model('To-do').create(newTodo, (err, todo) => {
+    if (err) {
+      return res.send('There was a problem adding the information to the database.');
+    }
+    res.redirect('/to-do');
+  });
+}
+
+function editToDoItem(req, res, next) {
+  const todoId = req.params.id;
+
+  const items = req.body.items.split('\n')
+    .map(item => item.trim())
+    .filter(item => item);
+
+  const findTodo = {
+    _id: todoId,
+  };
+
+  const updatedTodo = {
+    items: items,
+  };
+
+  mongoose.model('To-do').update(findTodo, updatedTodo, err => {
+    if (err) {
+      return res.send('There was a problem updating the information to the database: ' + err);
+    }
+
+    res.redirect('/to-do');
+  });
+}
