e080df6b83ac1a66d7125181a6d4bd4acad816c2
Anna Peterson <peterson.anna.c@gmail.com>
2019-09-02 16:01:41 -0700

    Refactor person checklist route

--- routes/person/checklist.js
+++ routes/person/checklist.js
@@ -8,122 +8,134 @@ module.exports = router;
 router.get('/person/:personId/checklist', personChecklist);
 
 function personChecklist(req, res) {
-  mongoose.model('Person')
-  .findById(req.personId)
-  .exec((err, person) => {
-    mongoose.model('Event')
+  const person = req.person;
+
+  withData(person, data => {
+    const checklistData = {};
+
+    checklistData.checklistLinks = getLinkChecklist(person.links);
+
+    let [birthYear, deathYear] = findBirthAndDeath(data.events);
+
+    checklistData.checklistLife = {
+      'Birth date': birthYear != null,
+      'Death date': deathYear != null,
+    };
+
+    checklistData.sourceChecklist = getSourceChecklist(data.sources,
+      person, birthYear, deathYear);
+
+    checklistData.incompleteSourceList = getIncompleteSources(data.sources);
+
+    res.render('layout', {
+      view: 'person/layout',
+      subview: 'checklist',
+      title: person.name,
+      paramPersonId: req.paramPersonId,
+      person: person,
+      ...checklistData
+    });
+  });
+}
+
+function withData(person, callback) {
+  mongoose.model('Event')
+  .find({ people: person })
+  .exec((err, events) => {
+    mongoose.model('Source')
     .find({ people: person })
-    .exec((err, events) => {
-      mongoose.model('Source')
-      .find({ people: person })
-      .populate('story')
-      .exec((err, sources) => {
-        var checklistLinks = {
-          Ancestry: null,
-          FamilySearch: null,
-          FindAGrave: null,
-          Lundberg: null,
-          WikiTree: null,
-        };
-
-        person.links.forEach((url) => {
-          if (url.match('lundbergancestry')) {
-            checklistLinks.Lundberg = url;
-          } else if (url.match('ancestry.com')) {
-            checklistLinks.Ancestry = url;
-          } else if (url.match('familysearch.org')) {
-            checklistLinks.FamilySearch = url;
-          } else if (url.match('findagrave')) {
-            checklistLinks.FindAGrave = url;
-          } else if (url.match('wikitree')) {
-            checklistLinks.WikiTree = url;
-          }
-        });
-
-        var checklistLife = {
-          'Birth date': false,
-          'Death date': false,
-        };
-
-        var birthYear = null;
-        var deathYear = null;
-
-        events.forEach((thisEvent) => {
-          if (thisEvent.title == 'birth') {
-            if (thisEvent.date != null && thisEvent.date.year != null) {
-              checklistLife['Birth date'] = true;
-              birthYear = thisEvent.date.year;
-            }
-          } else if (thisEvent.title == 'death') {
-            if (thisEvent.date != null && thisEvent.date.year != null) {
-              checklistLife['Death date'] = true;
-              deathYear = thisEvent.date.year;
-            }
-          }
-        });
-
-        var sourceChecklist = {};
-
-        sourceChecklist['grave'] = sources.filter((thisSource) => {
-          return thisSource.type == 'grave';
-        }).length > 0;
-
-        for (var year = 1840; year <= 1940; year += 10) {
-          if (birthYear != null && birthYear > year) {
-            continue;
-          }
-          if (deathYear == null) {
-            if (birthYear != null && year - birthYear > 90) {
-              continue;
-            }
-          } else if (deathYear < year) {
-            continue;
-          }
-          var sourceName = 'Census USA ' + year;
-          sourceChecklist[sourceName] = false;
-          sources.forEach((thisSource) => {
-            if (thisSource.story.title == sourceName) {
-              sourceChecklist[sourceName] = true;
-            }
-          });
-        }
-
-        if (birthYear != null && birthYear < 1900
-            && deathYear != null && deathYear > 1917) {
-          var sourceName = 'World War I draft';
-          sourceChecklist[sourceName] = false;
-          sources.forEach((thisSource) => {
-            if (thisSource.story.title == sourceName) {
-              sourceChecklist[sourceName] = true;
-            }
-          });
-        }
-
-        if (birthYear != null && birthYear < 1925
-            && deathYear != null && deathYear > 1940) {
-          var sourceName = 'World War II draft';
-          sourceChecklist[sourceName] = false;
-          sources.forEach((thisSource) => {
-            if (thisSource.story.title == sourceName) {
-              sourceChecklist[sourceName] = true;
-            }
-          });
-        }
-
-        res.render('layout', {
-          view: 'person/layout',
-          subview: 'checklist',
-          title: person.name,
-          paramPersonId: req.paramPersonId,
-          person: person,
-          checklistLinks: checklistLinks,
-          checklistLife: checklistLife,
-          sourceChecklist: sourceChecklist,
-          incompleteSourceList: getIncompleteSources(sources),
-        });
+    .populate('story')
+    .exec((err, sources) => {
+      callback({
+        events: events,
+        sources: sources,
       });
     });
   });
+}
+
+function getLinkChecklist(links) {
+  const checklistLinks = {
+    Ancestry: null,
+    FamilySearch: null,
+    FindAGrave: null,
+    Lundberg: null,
+    WikiTree: null,
+  };
+
+  links.forEach(url => {
+    if (url.match('lundbergancestry')) {
+      checklistLinks.Lundberg = url;
+    } else if (url.match('ancestry.com')) {
+      checklistLinks.Ancestry = url;
+    } else if (url.match('familysearch.org')) {
+      checklistLinks.FamilySearch = url;
+    } else if (url.match('findagrave')) {
+      checklistLinks.FindAGrave = url;
+    } else if (url.match('wikitree')) {
+      checklistLinks.WikiTree = url;
+    }
+  });
+
+  return checklistLinks;
+}
+
+function findBirthAndDeath(events) {
+  let birthYear, deathYear;
+
+  events.forEach(event => {
+    if (event.title == 'birth') {
+      if (event.date != null && event.date.year != null) {
+        birthYear = event.date.year;
+      }
+    } else if (event.title == 'death') {
+      if (event.date != null && event.date.year != null) {
+        deathYear = event.date.year;
+      }
+    }
+  });
+
+  return [birthYear, deathYear];
+}
+
+function getSourceChecklist(sources, person, birthYear, deathYear) {
+  const sourceChecklist = {};
+
+  const checkForStory = (attr, value) => {
+    sourceChecklist[value] = sources.filter(source => {
+      return source.story[attr] == value;
+    }).length > 0;
+  };
+
+  checkForStory('type', 'cemetery');
+
+  for (let year = 1840; year <= 1940; year += 10) {
+    if (birthYear != null && birthYear > year) {
+      continue;
+    }
+
+    if (deathYear == null) {
+      if (birthYear != null && year - birthYear > 90) {
+        continue;
+      }
+    } else if (deathYear < year) {
+      continue;
+    }
+
+    checkForStory('title', 'Census USA ' + year);
+  }
+
+  if (birthYear != null && deathYear != null
+      && !person.tags.includes('gender=female')) {
+    if (birthYear < 1900 && deathYear > 1917) {
+      checkForStory('title', 'World War I draft');
+    }
+    if (birthYear < 1925 && deathYear > 1940) {
+      checkForStory('title', 'World War II draft');
+    }
+  }
+
+  return sourceChecklist;
 }
 
 function getIncompleteSources(sourceList) {
@@ -133,15 +145,16 @@ function getIncompleteSources(sourceList) {
     const needsContent = source.content == null || source.content == '';
     const needsImage = source.images.length == 0;
     const needsSummary = (source.summary || '').length == 0;
-    const isCensus = source.type == 'document' && source.story.title.match('Census');
+    const isCensus = source.story.type == 'document'
+      && source.story.title.match('Census');
 
     let text1, text2;
 
-    if (source.type == 'newspaper') {
+    if (source.story.type == 'newspaper') {
       text1 = 'newspaper article: ' + source.title;
-    } else if (source.type == 'grave' || isCensus) {
+    } else if (source.story.type == 'cemetery' || isCensus) {
       text1 = source.story.title;
-    } else if (source.type == 'book') {
+    } else if (source.story.type == 'book') {
       text1 = source.story.title + ' - ' + source.title;
     } else {
       return;
@@ -152,10 +165,10 @@ function getIncompleteSources(sourceList) {
     if (needsContent) {
       missing.push('transcription');
     }
-    if (needsImage && source.type != 'book') {
+    if (needsImage && source.story.type != 'book') {
       missing.push('image');
     }
-    if (needsSummary && (isCensus || source.type == 'newspaper')) {
+    if (needsSummary && (isCensus || source.story.type == 'newspaper')) {
       missing.push('summary');
     }
 
