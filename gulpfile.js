361b1a2f93b89f28cf206f2fada934cd72c7fe15
Anna Peterson <peterson.anna.c@gmail.com>
2019-09-08 10:18:31 -0700

    Add gulp task to find all files for shared app

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const inject = require('gulp-inject');
 
-// gulp.task('default', done => {
-//   done();
-// });
+gulp.task('default', done => {
+  const indexFile = gulp.src('./shared/index.html');
+  const mainAppFiles = gulp.src(['./shared/app/*.js', './shared/app/**/*.js'],
+    { read: false });
+
+  indexFile
+  .pipe(inject(mainAppFiles, { ignorePath: 'shared', addRootSlash: false }))
+  .pipe(gulp.dest('shared/'));
+
+  done();
+});
 
 gulp.task('compress', done => {
   gulp.src(['./shared/app/*.js'])
