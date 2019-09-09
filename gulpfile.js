32073386062474d411a8aba02267c33447680392
Anna Peterson <peterson.anna.c@gmail.com>
2019-09-08 17:28:51 -0700

    Add gulp tasks for injecting raw or compressed JS

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,40 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const inject = require('gulp-inject');
 
-gulp.task('default', done => {
+const paths = {
+  mainApp: ['./shared/app/*.js', './shared/app/**/*.js'],
+  compressApp: './shared/all-app.js',
+};
+
+gulp.task('inject-all-shared-app-files', () => {
   const indexFile = gulp.src('./shared/index.html');
-  const mainAppFiles = gulp.src(['./shared/app/*.js', './shared/app/**/*.js'],
-    { read: false });
+  const jsFiles = gulp.src(paths.mainApp, { read: false });
 
-  indexFile
-  .pipe(inject(mainAppFiles, { ignorePath: 'shared', addRootSlash: false }))
+  return indexFile
+  .pipe(inject(jsFiles, { ignorePath: 'shared', addRootSlash: false }))
   .pipe(gulp.dest('shared/'));
-
-  done();
 });
 
-gulp.task('compress', done => {
-  gulp.src(['./shared/app/*.js'])
-    .pipe(concat('./shared/all-app.js'))
-    .pipe(gulp.dest('./'));
-  done();
+gulp.task('compress-shared-app-files', () => {
+  return gulp.src(paths.mainApp)
+  .pipe(concat(paths.compressApp))
+  .pipe(gulp.dest('./'));
 });
+
+gulp.task('inject-compressed-shared-app', () => {
+  const indexFile = gulp.src('./shared/index.html');
+  const jsFiles = gulp.src(paths.compressApp, { read: false });
+
+  return indexFile
+  .pipe(inject(jsFiles, { ignorePath: 'shared', addRootSlash: false }))
+  .pipe(gulp.dest('shared/'));
+});
+
+gulp.task('dev', gulp.series(
+  'inject-all-shared-app-files'
+));
+
+gulp.task('prod', gulp.series(
+  'compress-shared-app-files',
+  'inject-compressed-shared-app'
+));
