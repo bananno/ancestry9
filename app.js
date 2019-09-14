8cdacff8a7ddbc2650112bc246d19681a3d14f17
Anna Peterson <peterson.anna.c@gmail.com>
2019-09-14 11:35:19 -0700

    Rename route file

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const routeFiles = [
   ['source/citation'],
   ['notation'],
   ['map'],
-  ['to-do'],
+  ['checklist'],
   ['database'],
 ];
 
@@ -42,7 +42,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
