1624a1ac2deb95876ed131e8977f506d29655376
Anna Peterson <peterson.anna.c@gmail.com>
2019-09-28 17:40:49 -0700

    Use gulp to compile CSS and test JS for shared app

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,39 @@ const inject = require('gulp-inject');
 
 const paths = {
   mainApp: ['./shared/app/*.js', './shared/app/**/*.js'],
+  tests: ['./shared/tests/*.js'],
+  style: ['./shared/stylesheets/*.css', './shared/stylesheets/**/*.css'],
   compressApp: './shared/all-app.js',
+  compressStyle: './shared/all-style.css',
 };
 
 gulp.task('inject-all-shared-app-files', () => {
   const indexFile = gulp.src('./shared/index.html');
-  const jsFiles = gulp.src(paths.mainApp, { read: false });
+  const jsFiles = gulp.src([...paths.mainApp, ...paths.tests], { read: false });
 
   return indexFile
   .pipe(inject(jsFiles, { ignorePath: 'shared', addRootSlash: false }))
   .pipe(gulp.dest('shared/'));
 });
 
+gulp.task('inject-all-shared-style-files', () => {
+  const indexFile = gulp.src('./shared/index.html');
+  const cssFiles = gulp.src(paths.style, { read: false });
+
+  return indexFile
+  .pipe(inject(cssFiles, { ignorePath: 'shared', addRootSlash: false }))
+  .pipe(gulp.dest('shared/'));
+});
+
 gulp.task('compress-shared-app-files', () => {
   return gulp.src(paths.mainApp)
   .pipe(concat(paths.compressApp))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('compress-shared-style-files', () => {
+  return gulp.src(paths.style)
+  .pipe(concat(paths.compressStyle))
   .pipe(gulp.dest('./'));
 });
 
@@ -31,11 +49,23 @@ gulp.task('inject-compressed-shared-app', () => {
   .pipe(gulp.dest('shared/'));
 });
 
+gulp.task('inject-compressed-shared-style', () => {
+  const indexFile = gulp.src('./shared/index.html');
+  const cssFiles = gulp.src(paths.compressStyle, { read: false });
+
+  return indexFile
+  .pipe(inject(cssFiles, { ignorePath: 'shared', addRootSlash: false }))
+  .pipe(gulp.dest('shared/'));
+});
+
 gulp.task('dev', gulp.series(
-  'inject-all-shared-app-files'
+  'inject-all-shared-app-files',
+  'inject-all-shared-style-files'
 ));
 
 gulp.task('prod', gulp.series(
   'compress-shared-app-files',
-  'inject-compressed-shared-app'
+  'compress-shared-style-files',
+  'inject-compressed-shared-app',
+  'inject-compressed-shared-style'
 ));
