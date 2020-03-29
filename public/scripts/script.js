a76385f43813aebe1e095f0908264ea4bfdefdbd
Anna Peterson <peterson.anna.c@gmail.com>
2020-03-29 12:14:52 -0700

    Replace filters with one reusable function

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -20,3 +20,35 @@ function toggleLocationOptions() {
 function confirmDeletion(itemType) {
   return confirm('Delete this ' + itemType + '?');
 }
+
+/*
+  createQuickFilter({
+    input: '#select_input_box',
+    items: '.select_items_to_toggle',
+    getItemText: $item => $item.text(), or
+    getItemText: $item => $item.attr('some_attr'),
+    startFocus: boolean, // whether to focus on input on page load
+    then: (searchWords) => callback, // run every time after filtering is done
+  });
+*/
+function createQuickFilter(options) {
+  const $filter = $(options.input);
+
+  if (options.startFocus) {
+    $filter.focus();
+  }
+
+  $filter.keyup(() => {
+    const searchWords = $filter.val().trim().toLowerCase().split(' ').filter(Boolean);
+
+    $(options.items).each((i, item) => {
+      const itemText = options.getItemText($(item)).toLowerCase();
+      const isMatch = !searchWords.some(word => !itemText.match(word));
+      $(item).toggle(isMatch);
+    });
+
+    if (options.then) {
+      options.then(searchWords);
+    }
+  });
+}
