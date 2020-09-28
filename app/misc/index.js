e97c83b6144c23defb3c37fbeb5b8c7dbd9da752
Anna Bidstrup <annabidstrup20@gmail.com>
2020-09-27 19:07:38 -0700

    Add view for people living during each year

--- app/misc/index.js
+++ app/misc/index.js
@@ -7,6 +7,7 @@ module.exports = createRoutes;
 
 function createRoutes(router) {
   router.get('/timeline', showTimeline);
+  router.get('/year/:year', showYear);
 }
 
 async function showTimeline(req, res) {
@@ -69,5 +70,53 @@ async function showTimeline(req, res) {
     yearsWithEvents,
     peopleBornPerYear,
     maxPeoplePerYear,
+  });
+}
+
+async function showYear(req, res) {
+  const year = parseInt(req.params.year);
+
+  const people = await Person.find();
+
+  await Person.populateBirthAndDeath(people);
+
+  const bornThisYear = [];
+  const diedThisYear = [];
+  const livedThisYear = [];
+
+  people.forEach(person => {
+    const birthYear = person.getBirthYear();
+    const deathYear = person.getDeathYear();
+
+    person.age = (birthYear && deathYear) ? (year - birthYear) : null;
+
+    // born this year - applicable even if the death date is missing
+    if (birthYear === year) {
+      bornThisYear.push(person);
+      // in case born & died within the same year
+      if (deathYear === year) {
+        diedThisYear.push(person);
+      }
+    // died this year - applicable even if the birth date is missing
+    } else if (deathYear === year) {
+      diedThisYear.push(person);
+    // lived during this year - only applicable if both dates are present
+    } else if (birthYear && deathYear && birthYear < year && deathYear > year) {
+      livedThisYear.push(person);
+    }
+  });
+
+  sorting.sortBy(diedThisYear, person => person.age);
+  sorting.sortBy(livedThisYear, person => person.age);
+
+  const livedThisYearR = livedThisYear.reverse();
+  const split = Math.round(livedThisYear.length / 2);
+
+  res.render('misc/year', {
+    year,
+    bornThisYear,
+    diedThisYear,
+    livedThisYear1: livedThisYearR.slice(0, split),
+    livedThisYear2: livedThisYearR.slice(split),
   });
 }
