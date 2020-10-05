14cea6d7a0e7c9ea1563c8828b38d16ffcee167a
Anna Bidstrup <annabidstrup20@gmail.com>
2020-10-04 20:25:12 -0700

    Add view for creating a descendants chart

--- app/person/profile.js
+++ app/person/profile.js
@@ -19,6 +19,7 @@ module.exports = {
     relatives: personRelatives,
     connection: personConnection,
     wikitree: personWikitree,
+    descendants: personDescendants,
   }
 };
 
@@ -155,4 +156,45 @@ async function personWikitree(req, res) {
   Source.sortByStory(sources);
 
   res.renderPersonProfile('wikitree', {sources});
+}
+
+async function personDescendants(req, res) {
+  const monthNames = [
+    null, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ];
+
+  const people = await Person.find();
+
+  await Person.populateBirthAndDeath(people);
+
+  const isLivingTag = await Tag.findOne({title: 'living'});
+
+  const marriageEvents = await Event.find({title: 'marriage'});
+
+  res.renderPersonProfile('descendants', {
+    people,
+    marriageEvents,
+    findPersonInList: Person.findInList,
+    getLifeDatesString,
+    formatEventDate,
+  });
+
+  function getLifeDatesString(person) {
+    const livingStr = person.tags.includes(isLivingTag._id) ? 'living' : undefined;
+    return [
+      formatEventDate(person.birth) || '?',
+      formatEventDate(person.death) || livingStr || '?',
+    ].join('-');
+  }
+
+  function formatEventDate(event) {
+    if (event && event.date) {
+      if (event.date.display) {
+        return event.date.display;
+      }
+      const {day, month, year} = event.date;
+      return [monthNames[month], day, year].filter(Boolean).join(' ');
+    }
+  }
 }
