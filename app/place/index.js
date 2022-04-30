951a15a71e7fcbaf125ec07d469f5a3051fd5ea1
Anna Bidstrup <annabidstrup20@gmail.com>
2022-04-30 16:02:21 -0700

    Update place view for 3-part locations

--- app/place/index.js
+++ app/place/index.js
@@ -7,25 +7,31 @@ module.exports = createRoutes;
 
 function createRoutes(router) {
   router.get('/place/:country/:region1/:region2/:city', renderShowPlace);
+  router.get('/place/:country/:region1/:city', renderShowPlace);
 }
 
 async function renderShowPlace(req, res) {
   const {country, region1, region2, city} = req.params;
   const locationObj = {country, region1, region2, city};
-  const title = [country, region1, region2, city].join(' / ');
+  const title = [country, region1, region2, city]
+    .map(piece => piece === '-' ? '...' : piece)
+    .join(' / ');
 
-  const events = await Event.find({
-    'location.country': country,
-    'location.region2': region2,
-    'location.region1': region1,
-    'location.city': city,
-  }).populate('people');
+  const eventFilter = {};
+
+  ['country', 'region1', 'region2', 'city'].forEach(locationPiece => {
+    if (locationObj[locationPiece] && locationObj[locationPiece] !== '-') {
+      eventFilter[`location.${locationPiece}`] = locationObj[locationPiece];
+    }
+  });
+
+  const events = await Event.find(eventFilter).populate('people');
 
   Event.sortByDate(events);
 
   res.render('place/show', {
     events,
     locationObj,
-    title: title + '/' + (events.length),
+    title,
   });
 }
